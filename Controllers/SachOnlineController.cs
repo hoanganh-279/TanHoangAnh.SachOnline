119dd798486bec1fd182730ae09ead6996b921b4
Tân Hoàng Anh <tanhoanganh2006a@gmail.com>
2026-03-10 16:27:06 +0700

    hoàn thành lap 2

--- Controllers/SachOnlineController.cs
+++ Controllers/SachOnlineController.cs
@@ -1,27 +1,23 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TanHoangAnh.SachOnline.Models;
 
 namespace TanHoangAnh.SachOnline.Controllers
 {
     public class SachOnlineController : Controller
     {
+
+        TanHoangAnh.SachOnline.Models.SachOnlineEntities db = new TanHoangAnh.SachOnline.Models.SachOnlineEntities();
         // GET: SachOnline
-        public ActionResult Index()
-        {
-            return View();
-        }
 
         public ActionResult ChuDePartial()
         {
-            return PartialView();
-        }
-
-        public ActionResult SachBanNhieuPartial()
-        {
-            return PartialView();
+            var listChuDe = db.CHUDEs.ToList();
+            return PartialView(listChuDe);
         }
 
         public ActionResult NavPartial()
@@ -36,12 +32,36 @@ namespace TanHoangAnh.SachOnline.Controllers
 
         public ActionResult NhaXuatBanPartial()
         {
-            return PartialView();
+            var listNXB = db.NHAXUATBANs.ToList();
+            return PartialView(listNXB);
         }
 
         public ActionResult FooterPartial()
         {
             return PartialView();
+        }
+
+        private List<TanHoangAnh.SachOnline.Models.SACH> LaySachMoi (int count)
+        {
+            return db.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+
+        public ActionResult Index()
+        {
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
+        }
+
+        private List<TanHoangAnh.SachOnline.Models.SACH> LaySachBanNhieu(int count)
+        {
+            return db.SACHes.OrderByDescending(a => a.SoLuongBan).Take(count).ToList();
+        }
+
+        public ActionResult SachBanNhieuPartial()
+        {
+            var listSachBanNhieu = LaySachBanNhieu(6);
+
+            return PartialView(listSachBanNhieu);
         }
     }
 }
