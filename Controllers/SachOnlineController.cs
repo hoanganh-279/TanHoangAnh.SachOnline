504d739531a7dfc1b7d96df8919926fea77cdc0f
ASUS <ASUS@LAPTOP-3J6T1I18>
2026-03-10 13:39:41 +0700

    Add project files.

---
+++ Controllers/SachOnlineController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TanHoangAnh.SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChuDePartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult SachBanNhieuPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult NhaXuatBanPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult FooterPartial()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TanHoangAnh.SachOnline.MvcApplication" Language="C#" %>
